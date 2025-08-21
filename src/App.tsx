e7a74a511bf0c1e88d7a51ea8f52a614840fb2a8
mohammaduzzal <aalp72142@gmail.com>
2025-08-21 22:14:23 +0600

    basic project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/layout/Footer"
+import Navbar from "./components/layout/Navbar"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
